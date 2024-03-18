4dfd57d97c6ded00311b79cd4c29324de3ddf4de
Keldan Campbell <theoinkinator@gmail.com>
2024-03-18 13:40:12 -0700

    first commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { createSignal } from "solid-js";
+import styles from  "./Counter.module.css"
+
+export default function Counter() {
+  const [count, setCount] = createSignal(0);
+  return (
+    <button class={styles.increment} onClick={() => setCount(count() + 1)}>
+      Clicks: {count()}
+    </button>
+  );
+}
